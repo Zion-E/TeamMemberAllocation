2c191916a0e90a0f1a5f24e995588c19220b145e
Idowu Akinsanmi <81622869+Zion-E@users.noreply.github.com>
2022-09-25 21:03:04 +0000

    Initial commit

---
+++ src/NotFound.jsx
@@ -0,0 +1,12 @@
+const NotFound = () => {
+  return (
+    <main className = "contaner">
+      <div className = "row justify-content-center mt-3 mb-4">
+        <div className = "col-8">
+          <h1 className="text-danger">404 - Page not found</h1>
+        </div>
+      </div>
+    </main>
+  )
+};
+export default NotFound;
